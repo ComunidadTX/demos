423d814e774bc6061554ad2b3294be0ae5d35e55
Miguel Torres <39393127+migueltorresio@users.noreply.github.com>
2019-03-14 00:06:08 -0600

    Día Pi

---
+++ pi.py
@@ -0,0 +1,20 @@
+n=1
+p=0
+import time
+mensaje1 = 'Pi = '
+
+
+sign=1.
+while n<10000:
+    
+    if n%2==0:
+        n=n+1
+    else:
+        p=p+4.*(sign/n)
+        n=n+1
+        sign=sign*(-1.)
+    
+
+pi = str(p)
+print('Pi = ' + ''+ pi)
+time.sleep(60)
